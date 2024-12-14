e7ccb946e57cbe302cba1c8b36d998e6868792d7
Kiran Hegde <kiransrivani@gmail.com>
2024-12-13 18:03:40 -0600

    DML and DQL half fix

--- src/main/java/com/neodymium/davisbase/services/queryprocessors/DMLProcessor.java
+++ src/main/java/com/neodymium/davisbase/services/queryprocessors/DMLProcessor.java
@@ -1,49 +1,46 @@
 package com.neodymium.davisbase.services.queryprocessors;
 
-import com.neodymium.davisbase.constants.enums.Constraints;
-import com.neodymium.davisbase.constants.enums.DataTypes;
-import com.neodymium.davisbase.models.table.Column;
 import com.neodymium.davisbase.models.table.Table;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Slf4j
 @Service
 @AllArgsConstructor
 public class DMLProcessor {
-    private static final Map<String,Object> query_table = new HashMap<>();
     public void process(String query) throws IOException {
         if (query.toUpperCase().startsWith("INSERT INTO")) {
-            String insertDefinition = query.substring("INSERT INTO".length()).trim();
-            insertIntoTable(insertDefinition);
+            insert(query);
         } else if (query.toUpperCase().startsWith("DELETE FROM")) {
-            String deleteDefinition = query.substring("DELETE FROM".length()).trim();
-            deleteFromTable(deleteDefinition);
+            delete(query);
+        } else if (query.toUpperCase().startsWith("UPDATE")) {
+            update(query);
         } else {
             System.out.println("Invalid command.");
         }
     }
 
-    public void insertIntoTable(String insertDefinition) throws IOException {
+    private void update(String query) {
+
+    }
+
+    public void insert(String query) throws IOException {
         String[] parts = insertDefinition.split("\\s+VALUES\\s+", 2);
         if (parts.length < 2) {
             throw new IllegalArgumentException("Invalid INSERT INTO syntax.");
         }
         String tableName = parts[0].trim();
         String valuesDefinition = parts[1].replace("(", "").replace(")", "").trim();
-        String [] values = valuesDefinition.split(",");
-        Table table = new Table(tableName, values); // Is this logic right for interaction with the table.java
-        table.insert(query_table.put(tableName,values)); // insert the hashmap object.
+        String[] values = valuesDefinition.split(",");
+        Table table = new Table(tableName, List.of());
+        table.insert(values);
     }
 
-    public void deleteFromTable(String deleteDefinition) throws IOException {
+    public void delete(String deleteDefinition) throws IOException {
         String[] parts = deleteDefinition.split("\\s+WHERE\\s+", 2);
         String tableName = parts[0].trim();
         String condition = parts.length > 1 ? parts[1].trim() : null;
--- src/main/java/com/neodymium/davisbase/services/queryprocessors/DQLProcessor.java
+++ src/main/java/com/neodymium/davisbase/services/queryprocessors/DQLProcessor.java
@@ -1,8 +1,5 @@
 package com.neodymium.davisbase.services.queryprocessors;
 
-import com.neodymium.davisbase.constants.enums.Constraints;
-import com.neodymium.davisbase.constants.enums.DataTypes;
-import com.neodymium.davisbase.models.table.Column;
 import com.neodymium.davisbase.models.table.Table;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,26 +7,50 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+
+import static com.neodymium.davisbase.constants.Constants.DATABASE_LINE_SEPARATOR;
+import static com.neodymium.davisbase.constants.Constants.TABLE_CATALOG_NAME;
 
 @Slf4j
 @Service
 @AllArgsConstructor
-
-
 public class DQLProcessor {
     public void process(String query) throws IOException {
         if (query.toUpperCase().startsWith("SELECT")) {
-            String selectQuery = query.substring("SELECT".length()).trim();
-            processSelect(selectQuery);
+            select(query);
+        } else if (query.toUpperCase().startsWith("SHOW")) {
+            select("select * from " + TABLE_CATALOG_NAME);
+        } else if (query.toUpperCase().startsWith("HELP")) {
+            help();
         } else {
             System.out.println("Invalid DQL command.");
         }
     }
-    // Process SELECT query and extract the relevant parts (columns, conditions)
-    public void processSelect(String query) throws IOException {
+
+    private void help() {
+        System.out.println(DATABASE_LINE_SEPARATOR);
+        System.out.println("SUPPORTED COMMANDS");
+        System.out.println("All commands below are case insensitive");
+        System.out.println();
+        System.out.println("\tSHOW TABLES;                                                               Display all the tables in the database.");
+        System.out.println("\tCREATE TABLE table_name (<column_name datatype constraint>);               Create a new table in the database.");
+        System.out.println("\tINSERT INTO table_name VALUES (value1,value2,..);                          Insert a new record into the table.");
+        System.out.println("\tDELETE FROM TABLE table_name WHERE column_name operator value;             Delete a record from the table whose rowid is <key_value>.");
+        System.out.println("\tUPDATE table_name SET column_name = value WHERE column_name operator value Modifies the records in the table.");
+        System.out.println("\tCREATE INDEX ON table_name (column_name);                                  Create index for the specified column in the table");
+        System.out.println("\tSELECT * FROM table_name;                                                  Display all records in the table.");
+        System.out.println("\tSELECT * FROM table_name WHERE column_name operator value;                 Display records in the table where the given condition is satisfied.");
+        System.out.println("\tDROP TABLE table_name;                                                     Remove table data and its schema.");
+        System.out.println("\tVERSION;                                                                   Show the program version.");
+        System.out.println("\tHELP;                                                                      Show this help information.");
+        System.out.println("\tEXIT;                                                                      Exit DavisBase.");
+        System.out.println();
+        System.out.println();
+        System.out.println(DATABASE_LINE_SEPARATOR);
+    }
+
+    public void select(String query) throws IOException {
         String[] parts = query.split("FROM", 2);
         if (parts.length < 2) {
             throw new IllegalArgumentException("Invalid SELECT query syntax.");
@@ -47,8 +68,6 @@ public class DQLProcessor {
 
         // If there's a WHERE clause, extract it
         String condition = tableParts.length > 1 ? tableParts[1].trim() : null;
-
-        // Retrieve the table and filter records
         Table table = new Table(tableName, null);
         List<Object> records = table.select(columns, condition);
 
